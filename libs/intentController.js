6af503408a7d6be09eae71ccc56bb97e406152e0
danic <dalbacarrasco@gmail.com>
2022-10-24 20:21:32 -0400

    fixed14 pedidos

--- libs/intentController.js
+++ libs/intentController.js
@@ -259,7 +259,6 @@ async function pizzaEspecifica(response, facebookId) {
 async function pedido(response, facebookId) {
   const pizzaDF = await response.parameters?.fields?.TipoPizza?.stringValue;
   const cantidad = await response.parameters?.fields?.number?.numberValue;
-  return `La respuesta es : ${cantidad}`;
   let cant = parseInt(cantidad);
 
   // validar que exista la pizza
